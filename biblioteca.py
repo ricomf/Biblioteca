0e30908010b8776f1f6bbf4a45d3a075af2e7995
ricomf <ricardojmf2005@gmail.com>
2025-02-05 11:53:37 -0300

    Bugfixed func delete book

--- biblioteca.py
+++ biblioteca.py
@@ -1,7 +1,8 @@
 import customtkinter as ctk
+from tkinter import messagebox
 
 class Livro:
-    livros = []
+    livros = [] # Array
 
     def __init__(self, titulo, autor, editora, ano, genero, paginas):
         self.titulo = titulo
@@ -12,20 +13,63 @@ class Livro:
         self.paginas = paginas
 
     @classmethod
-    def cadastrar(cls):
-        titulo = ctk.CTkInputDialog(text="Digite o título do livro:", title="Cadastro de Livro").get_input()
-        autor = ctk.CTkInputDialog(text="Digite o autor do livro:", title="Cadastro de Livro").get_input()
-        editora = ctk.CTkInputDialog(text="Digite a editora do livro:", title="Cadastro de Livro").get_input()
-        ano = ctk.CTkInputDialog(text="Digite o ano de publicação do livro:", title="Cadastro de Livro").get_input()
-        genero = ctk.CTkInputDialog(text="Digite o gênero do livro:", title="Cadastro de Livro").get_input()
-        paginas = ctk.CTkInputDialog(text="Digite o número de páginas do livro:", title="Cadastro de Livro").get_input()
-        
-        novo_livro = Livro(titulo, autor, editora, ano, genero, paginas)
-        cls.livros.append(novo_livro)
-        cls.show_message("Cadastro de Livro", f"O livro '{titulo}' foi cadastrado com sucesso!")
+    def cadastrar(cls, parent_frame, main_frame): # Cadastrar livro
+        '''Função para cadastrar um novo livro.'''
+        def submit():
+            '''Função para submeter o cadastro do livro.'''
+            titulo = entry_titulo.get()
+            autor = entry_autor.get()
+            editora = entry_editora.get()
+            ano = entry_ano.get()
+            genero = entry_genero.get()
+            paginas = entry_paginas.get()
+            
+            novo_livro = Livro(titulo, autor, editora, ano, genero, paginas)
+            cls.livros.append(novo_livro)
+            messagebox.showinfo("Cadastro de Livro", f"O livro '{titulo}' foi cadastrado com sucesso!")
+            cadastro_frame.pack_forget()
+            main_frame.pack(fill="both", expand=True)
+
+        parent_frame.pack_forget()
+        cadastro_frame = ctk.CTkFrame(parent_frame.master)
+        cadastro_frame.pack(fill="both", expand=True)
+
+        label_titulo = ctk.CTkLabel(cadastro_frame, text="Título:")
+        label_titulo.pack(pady=5)
+        entry_titulo = ctk.CTkEntry(cadastro_frame)
+        entry_titulo.pack(pady=5)
+
+        label_autor = ctk.CTkLabel(cadastro_frame, text="Autor:")
+        label_autor.pack(pady=5)
+        entry_autor = ctk.CTkEntry(cadastro_frame)
+        entry_autor.pack(pady=5)
+
+        label_editora = ctk.CTkLabel(cadastro_frame, text="Editora:")
+        label_editora.pack(pady=5)
+        entry_editora = ctk.CTkEntry(cadastro_frame)
+        entry_editora.pack(pady=5)
+
+        label_ano = ctk.CTkLabel(cadastro_frame, text="Ano:")
+        label_ano.pack(pady=5)
+        entry_ano = ctk.CTkEntry(cadastro_frame)
+        entry_ano.pack(pady=5)
+
+        label_genero = ctk.CTkLabel(cadastro_frame, text="Gênero:")
+        label_genero.pack(pady=5)
+        entry_genero = ctk.CTkEntry(cadastro_frame)
+        entry_genero.pack(pady=5)
+
+        label_paginas = ctk.CTkLabel(cadastro_frame, text="Páginas:")
+        label_paginas.pack(pady=5)
+        entry_paginas = ctk.CTkEntry(cadastro_frame)
+        entry_paginas.pack(pady=5)
+
+        btn_submit = ctk.CTkButton(cadastro_frame, text="Cadastrar", command=submit)
+        btn_submit.pack(pady=20)
 
     @classmethod
-    def listar_livros(cls):
+    def listar_livros(cls): # Listar livros
+        '''Função para listar os livros cadastrados.'''
         if not cls.livros:
             cls.show_message("Listar Livros", "Nenhum livro cadastrado.")
         else:
@@ -33,7 +77,8 @@ class Livro:
             cls.show_message("Listar Livros", livros_str)
 
     @classmethod
-    def pesquisar_livro(cls):
+    def pesquisar_livro(cls): # Pesquisar livro
+        '''Função para pesquisar um livro pelo título.'''
         titulo = ctk.CTkInputDialog(text="Digite o título do livro que deseja pesquisar:", title="Pesquisar Livro").get_input()
         for livro in cls.livros:
             if livro.titulo == titulo:
@@ -42,10 +87,11 @@ class Livro:
         cls.show_message("Pesquisar Livro", f"O livro '{titulo}' não foi encontrado.")
     
     @classmethod
-    def deletar_livro(cls):
+    def deletar_livro(cls): # Deletar livro
+        '''Função para deletar um livro pelo título.'''
         titulo = ctk.CTkInputDialog(text="Digite o título do livro que deseja deletar:", title="Deletar Livro").get_input()
         for livro in cls.livros:
-            if livro.titulo == titulo.title():
+            if livro.titulo == titulo:
                 cls.livros.remove(livro)
                 cls.show_message("Deletar Livro", f"O livro '{titulo}' foi deletado com sucesso.")
                 return
--- index.py
+++ index.py
@@ -10,21 +10,23 @@ def main():
     janela.title("Sistema de Biblioteca")
     janela.geometry("600x400")
 
+    main_frame = ctk.CTkFrame(janela)
+    main_frame.pack(fill="both", expand=True)
 
     '''Botões'''
-    btn_cadastrar = ctk.CTkButton(janela, text="Cadastrar Livro", command=Livro.cadastrar, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
+    btn_cadastrar = ctk.CTkButton(main_frame, text="Cadastrar Livro", command=lambda: Livro.cadastrar(main_frame, main_frame), fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
     btn_cadastrar.pack(pady=20)
 
-    btn_listar = ctk.CTkButton(janela, text="Listar Livros", command=Livro.listar_livros, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
+    btn_listar = ctk.CTkButton(main_frame, text="Listar Livros", command=Livro.listar_livros, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
     btn_listar.pack(pady=20)
     
-    btn_pesquisar = ctk.CTkButton(janela, text="Pesquisar Livros", command=Livro.pesquisar_livro, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
+    btn_pesquisar = ctk.CTkButton(main_frame, text="Pesquisar Livros", command=Livro.pesquisar_livro, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
     btn_pesquisar.pack(pady=20)
 
-    btn_deletar = ctk.CTkButton(janela, text="Deletar Livro", command=Livro.deletar_livro, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
+    btn_deletar = ctk.CTkButton(main_frame, text="Deletar Livro", command=Livro.deletar_livro, fg_color="gray20", hover_color="gray30", border_color="gray20", border_width=0)
     btn_deletar.pack(pady=20)
 
-    btn_sair = ctk.CTkButton(janela, text="Sair", command=janela.quit, fg_color="gray20", hover_color="gray30")
+    btn_sair = ctk.CTkButton(main_frame, text="Sair", command=janela.quit, fg_color="gray20", hover_color="gray30")
     btn_sair.pack(pady=30)
 
     janela.mainloop()
